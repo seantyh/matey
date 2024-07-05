eed3a81f350c4e6fc2154ec44e1d9c8723c9f76e
seantyh <seantyh@gmail.com>
2024-07-05 17:11:33 +0200

    add vec_db.py

--- src/matey/__init__.py
+++ src/matey/__init__.py
@@ -2,4 +2,5 @@ from .llm_api import llm, chatgpt, sonnet, haiku, Session
 from .ipynb_api import load_notebook, to_rag
 from .git_utils import get_git_commits, get_file_history, recently_worked_on_files
 from .db import db_put, db_get, db_delete, db_query
+from .vec_db import VecDB
 from .agent import Agent, NotebookAgent
